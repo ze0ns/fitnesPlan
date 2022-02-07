0206bcc0fcf61f654ce52b9cab0f8f696d03db3a
MarkDestroyer <markkir123@gmail.com>
2022-02-07 23:20:58 +0300

    Swift
    Redesign alerts.

--- Sport 2022/API-Controllers/LoginViewController.swift
+++ Sport 2022/API-Controllers/LoginViewController.swift
@@ -7,6 +7,7 @@
 
 import Strapi_iOS
 import UIKit
+import SwiftEntryKit
 
 class LoginViewController: UIViewController {
     let strapi = Strapi.shared
@@ -52,6 +53,198 @@ class LoginViewController: UIViewController {
         self.ScrollView?.endEditing(true)
     }
 
+    
+    func setupAttributes() -> EKAttributes {
+        var attributes = EKAttributes.bottomFloat
+        attributes.displayDuration = .infinity
+        attributes.screenBackground = .color(color: .init(light: UIColor(white: 100.0/255.0, alpha: 0.3), dark: UIColor(white: 50.0/255.0, alpha: 0.3)))
+        attributes.shadow = .active(
+            with: .init(
+                color: .black,
+                opacity: 0.3,
+                radius: 8
+            )
+        )
+        
+        attributes.entryBackground = .color(color: .standardBackground)
+        attributes.roundCorners = .all(radius: 25)
+        
+        attributes.screenInteraction = .dismiss
+        attributes.entryInteraction = .absorbTouches
+        attributes.scroll = .enabled(
+            swipeable: true,
+            pullbackAnimation: .jolt
+        )
+        
+        attributes.entranceAnimation = .init(
+            translate: .init(
+                duration: 0.7,
+                spring: .init(damping: 1, initialVelocity: 0)
+            ),
+            scale: .init(
+                from: 1.05,
+                to: 1,
+                duration: 0.4,
+                spring: .init(damping: 1, initialVelocity: 0)
+            )
+        )
+        
+        attributes.exitAnimation = .init(
+            translate: .init(duration: 0.2)
+        )
+        attributes.popBehavior = .animated(
+            animation: .init(
+                translate: .init(duration: 0.2)
+            )
+        )
+        
+        attributes.positionConstraints.verticalOffset = 10
+        attributes.statusBar = .dark
+        return attributes
+    }
+    
+    func setupMessage() -> EKPopUpMessage {
+        
+        let image = UIImage(named: "40")!.withRenderingMode(.alwaysOriginal)
+        let title = "Ошибка!"
+        let description =
+        """
+        Введите логин и пароль
+        """
+        
+        let themeImage = EKPopUpMessage.ThemeImage(image: EKProperty.ImageContent(image: image, size: CGSize(width: 60, height: 60), tint: .black, contentMode: .scaleAspectFit))
+        
+        let titleLabel = EKProperty.LabelContent(text: title, style: .init(font: UIFont.systemFont(ofSize: 24),
+                                                                      color: .black,
+                                                                      alignment: .center))
+        
+        let descriptionLabel = EKProperty.LabelContent(
+            text: description,
+            style: .init(
+                font: UIFont.systemFont(ofSize: 16),
+                color: .black,
+                alignment: .center
+            )
+        )
+        
+        let button = EKProperty.ButtonContent(
+            label: .init(
+                text: "ОК",
+                style: .init(
+                    font: UIFont.systemFont(ofSize: 16),
+                    color: .black
+                )
+            ),
+            backgroundColor: .init(UIColor.systemRed),
+            highlightedBackgroundColor: .clear
+        )
+        
+        let message = EKPopUpMessage(themeImage: themeImage, title: titleLabel, description: descriptionLabel, button: button) {
+            SwiftEntryKit.dismiss()
+        }
+        return message
+    }
+    
+    
+    func setupMessage2() -> EKPopUpMessage {
+        
+        let image = UIImage(named: "40")!.withRenderingMode(.alwaysOriginal)
+        let title = "Ошибка!"
+        let description =
+        """
+        Введите логин
+        """
+        
+        let themeImage = EKPopUpMessage.ThemeImage(image: EKProperty.ImageContent(image: image, size: CGSize(width: 60, height: 60), tint: .black, contentMode: .scaleAspectFit))
+        
+        let titleLabel = EKProperty.LabelContent(text: title, style: .init(font: UIFont.systemFont(ofSize: 24),
+                                                                      color: .black,
+                                                                      alignment: .center))
+        
+        let descriptionLabel = EKProperty.LabelContent(
+            text: description,
+            style: .init(
+                font: UIFont.systemFont(ofSize: 16),
+                color: .black,
+                alignment: .center
+            )
+        )
+        
+        let button = EKProperty.ButtonContent(
+            label: .init(
+                text: "ОК",
+                style: .init(
+                    font: UIFont.systemFont(ofSize: 16),
+                    color: .black
+                )
+            ),
+            backgroundColor: .init(UIColor.systemRed),
+            highlightedBackgroundColor: .clear
+        )
+        
+        let message = EKPopUpMessage(themeImage: themeImage, title: titleLabel, description: descriptionLabel, button: button) {
+            SwiftEntryKit.dismiss()
+        }
+        return message
+    }
+    
+    
+    func setupMessage3() -> EKPopUpMessage {
+        
+        let image = UIImage(named: "40")!.withRenderingMode(.alwaysOriginal)
+        let title = "Ошибка!"
+        let description =
+        """
+        Введите пароль
+        """
+        
+        let themeImage = EKPopUpMessage.ThemeImage(image: EKProperty.ImageContent(image: image, size: CGSize(width: 60, height: 60), tint: .black, contentMode: .scaleAspectFit))
+        
+        let titleLabel = EKProperty.LabelContent(text: title, style: .init(font: UIFont.systemFont(ofSize: 24),
+                                                                      color: .black,
+                                                                      alignment: .center))
+        
+        let descriptionLabel = EKProperty.LabelContent(
+            text: description,
+            style: .init(
+                font: UIFont.systemFont(ofSize: 16),
+                color: .black,
+                alignment: .center
+            )
+        )
+        
+        let button = EKProperty.ButtonContent(
+            label: .init(
+                text: "ОК",
+                style: .init(
+                    font: UIFont.systemFont(ofSize: 16),
+                    color: .black
+                )
+            ),
+            backgroundColor: .init(UIColor.systemRed),
+            highlightedBackgroundColor: .clear
+        )
+        
+        let message = EKPopUpMessage(themeImage: themeImage, title: titleLabel, description: descriptionLabel, button: button) {
+            SwiftEntryKit.dismiss()
+        }
+        return message
+    }
+    
+    
+    @objc func handleShowPopUp() {
+        SwiftEntryKit.display(entry: MyPopUpView(with: setupMessage()), using: setupAttributes())
+    }
+    
+    @objc func handleShowPopUp2() {
+        SwiftEntryKit.display(entry: MyPopUpView(with: setupMessage2()), using: setupAttributes())
+    }
+    
+    @objc func handleShowPopUp3() {
+        SwiftEntryKit.display(entry: MyPopUpView(with: setupMessage3()), using: setupAttributes())
+    }
+
+    
 
     // Когда клавиатура появляется
     @objc func keyboardWasShown(notification: Notification) {
@@ -116,49 +309,16 @@ class LoginViewController: UIViewController {
            }
         } else if EmailTextField.text == ""  && PassWordTextField.text == "" {
             print("введите данные")
-            showAlertPassWordAndEmail()
+            handleShowPopUp()
         } else if EmailTextField.text == "" {
-            showAlertEmail()
+            handleShowPopUp2()
         } else if PassWordTextField.text == "" {
-            showAlertPassWord()
+            handleShowPopUp3()
         }
     }
     
-    func showAlertEmail() {
-
-            // create the alert
-            let alert = UIAlertController(title: "Ошибка", message: "Введите логин.", preferredStyle: UIAlertController.Style.alert)
-
-            // add an action (button)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-
-            // show the alert
-            self.present(alert, animated: true, completion: nil)
-        }
     
-    func showAlertPassWord() {
-
-            // create the alert
-            let alert = UIAlertController(title: "Ошибка", message: "Введите пароль.", preferredStyle: UIAlertController.Style.alert)
-
-            // add an action (button)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-
-            // show the alert
-            self.present(alert, animated: true, completion: nil)
-        }
     
-    func showAlertPassWordAndEmail() {
-
-            // create the alert
-            let alert = UIAlertController(title: "Ошибка", message: "Введите логин и пароль.", preferredStyle: UIAlertController.Style.alert)
-
-            // add an action (button)
-            alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
-
-            // show the alert
-            self.present(alert, animated: true, completion: nil)
-        }
     
 
     
@@ -198,11 +358,11 @@ class LoginViewController: UIViewController {
                  }
               } else if EmailTextField.text == ""  && PassWordTextField.text == "" {
                   print("введите данные")
-                  showAlertPassWordAndEmail()
+                  handleShowPopUp()
               } else if EmailTextField.text == "" {
-                  showAlertEmail()
+                  handleShowPopUp2()
               } else if PassWordTextField.text == "" {
-                  showAlertPassWord()
+                  handleShowPopUp3()
        }
     }
 }
